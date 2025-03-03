1525eb43d2397922f1f54254c81c8f6287117f32
Akashdeep Singh <akashdeepsingh2kk@gmail.com>
2025-03-02 17:53:45 -0800

    Initial commit

---
+++ src/main/java/com/pl/premier_zone/player/Player.java
@@ -0,0 +1,205 @@
+package com.pl.premier_zone.player;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="players_stats")  // Updated table name to match database
+public class Player {
+    @Id
+    @Column(name = "name", unique = true)
+    private String name;
+
+    @Column(name = "nation")
+    private String nation;
+
+    @Column(name = "position")
+    private String position;
+
+    @Column(name = "age")
+    private Integer age;
+
+    @Column(name = "mp")
+    private Integer mp;
+
+    @Column(name = "starts")
+    private Integer starts;
+
+    @Column(name = "min")
+    private Double min;
+
+    @Column(name = "gls")
+    private Double gls;
+
+    @Column(name = "ast")
+    private Double ast;
+
+    @Column(name = "pk")
+    private Double pk;
+
+    @Column(name = "crdy")
+    private Double crdy;
+
+    @Column(name = "crdr")
+    private Double crdr;
+
+    @Column(name = "xg")
+    private Double xg;
+
+    @Column(name = "xag")
+    private Double xag;
+
+    @Column(name = "team")
+    private String team;
+
+    // Default constructor
+    public Player() {
+    }
+
+    // Full constructor
+    public Player(String name, String nation, String position, Integer age, Integer mp, Integer starts, Double min, Double gls, Double ast, Double pk, Double crdy, Double crdr, Double xg, Double xag, String team) {
+        this.name = name;
+        this.nation = nation;
+        this.position = position;
+        this.age = age;
+        this.mp = mp;
+        this.starts = starts;
+        this.min = min;
+        this.gls = gls;
+        this.ast = ast;
+        this.pk = pk;
+        this.crdy = crdy;
+        this.crdr = crdr;
+        this.xg = xg;
+        this.xag = xag;
+        this.team = team;
+    }
+
+    // Constructor for primary key only
+    public Player(String name) {
+        this.name = name;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNation() {
+        return nation;
+    }
+
+    public void setNation(String nation) {
+        this.nation = nation;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getMp() {
+        return mp;
+    }
+
+    public void setMp(Integer mp) {
+        this.mp = mp;
+    }
+
+    public Integer getStarts() {
+        return starts;
+    }
+
+    public void setStarts(Integer starts) {
+        this.starts = starts;
+    }
+
+    public Double getMin() {
+        return min;
+    }
+
+    public void setMin(Double min) {
+        this.min = min;
+    }
+
+    public Double getGls() {
+        return gls;
+    }
+
+    public void setGls(Double gls) {
+        this.gls = gls;
+    }
+
+    public Double getAst() {
+        return ast;
+    }
+
+    public void setAst(Double ast) {
+        this.ast = ast;
+    }
+
+    public Double getPk() {
+        return pk;
+    }
+
+    public void setPk(Double pk) {
+        this.pk = pk;
+    }
+
+    public Double getCrdy() {
+        return crdy;
+    }
+
+    public void setCrdy(Double crdy) {
+        this.crdy = crdy;
+    }
+
+    public Double getCrdr() {
+        return crdr;
+    }
+
+    public void setCrdr(Double crdr) {
+        this.crdr = crdr;
+    }
+
+    public Double getXg() {
+        return xg;
+    }
+
+    public void setXg(Double xg) {
+        this.xg = xg;
+    }
+
+    public Double getXag() {
+        return xag;
+    }
+
+    public void setXag(Double xag) {
+        this.xag = xag;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+}
